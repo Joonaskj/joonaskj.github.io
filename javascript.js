eb3509798536485cfc3cec38612393934bf7497e
Joonaskj <joonas.taskula@gmail.com>
2021-11-27 21:38:13 +0200

    project

---
+++ javascript.js
@@ -0,0 +1,10 @@
+/*function classFunction() {
+    if (window.innerWidth < 1800){
+        document.getElementById("img1").classList.remove("order-first")
+        document.getElementById("img1").classList.add("order-last")       
+    }
+    else {
+        document.getElementById("img1").classList.remove("order-last")
+        document.getElementById("img1").classList.add("order-first")       
+    }
+}
